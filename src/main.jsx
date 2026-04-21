299a2277944653f587f36b98daa3a96e965d520d
miles <mdnrosen@hotmail.co.uk>
2026-04-21 21:47:40 +0100

    corrected incorrect team total wins

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App";
 import "./style.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React>
+  <React.Fragment>
     <App />
-  </React>,
+  </React.Fragment>,
 );
